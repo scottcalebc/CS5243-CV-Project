691e9661f0e1a4a121b4037f006188701977e3ab
C. Caleb Scott <caleb.scott@my.utsa.edu>
2023-11-11 16:52:39 -0600

    Test download

--- test_download.py
+++ test_download.py
@@ -3,17 +3,24 @@
 
 import os
 from pathlib import Path
+import shutil
 
-from ultralytics.utils.downloads import download, unzip_file
+from ultralytics.utils.downloads import safe_download, unzip_file
 
 
-dir = "./datasets/EikelboomSavana"
+top_dataset_dir = "./datasets"
+dataset_dir = os.path.join(top_dataset_dir, "EikelboomSavana")
+Path()
 urls = [
         #'https://data.4tu.nl/file/9b1a2fcb-930e-4cc5-b9f0-a381dd1c7206/f978a7a0-f2aa-4c2b-a663-1165b247b56a',
         'https://data.4tu.nl/ndownloader/items/9b1a2fcb-930e-4cc5-b9f0-a381dd1c7206/versions/1'
         ]
 
-download(urls, dir=dir, curl=True, threads=4)
+safe_download(urls[0], file="EikelboomSavana.zip", dir=top_dataset_dir, unzip=True, delete=True, curl=True, progress=False)
 
-# if os.path.exists(os.path.join(dir, "f978a7a0-f2aa-4c2b-a663-1165b247b56a")):
-#     unzip_file(os.path.join(dir, "f978a7a0-f2aa-4c2b-a663-1165b247b56a"))
+if os.path.exists(os.path.join(dataset_dir, "data.zip")):
+    data_unzip_dir = unzip_file(os.path.join(dataset_dir, "data.zip"), dataset_dir)
+
+    if data_unzip_dir != dataset_dir:
+        shutil.copytree(data_unzip_dir, dataset_dir)
+        shutil.rmtree(data_unzip_dir)
