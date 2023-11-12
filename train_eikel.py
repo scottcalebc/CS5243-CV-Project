035c406dc7e60486880fb47fd38e8f3a92187f57
C. Caleb Scott <caleb.scott@my.utsa.edu>
2023-11-12 17:18:55 -0600

    Adjust training to fit within hardware constraint

--- train_eikel.py
+++ train_eikel.py
@@ -5,7 +5,12 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
 
-model.train(data="data/EikelboomSavanna.yaml", epochs=10, imgsz=(3452,5184))
+
+# Would like to train on default resolution of training images, but this requries lots of memory
+#model.train(data="data/EikelboomSavanna.yaml", epochs=10, imgsz=(3452,5184))
+
+# This trains on default size of 640, model.train performs resizing of input images automagically
+model.train(data="data/EikelboomSavanna.yaml", epochs=10)
 
 
 
